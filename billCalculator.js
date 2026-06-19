a2dc504d0747b150b93635c18580163a531fbbdd
Lily <ssu627366@gmail.com>
2026-06-19 12:30:03 +0630

    new

--- billCalculator.js
+++ billCalculator.js
@@ -5,22 +5,26 @@ let list = [
   { name: "Dessert", price: 5.75 },
 ];
 // total bill
-let total_Price = 0;
-for (let i = 0; i < list.length; i++) {
-  total_Price += list[i].price;
+function billCal(listitem) {
+  let totalPrice = 0;
+  for (let i = 0; i < listitem.length; i++) {
+    totalPrice += listitem[i].price;
+  }
+  console.log(`The total bill : $${totalPrice}`);
+  // service Charge
+  let service = 0.1;
+  let serviceCharge = totalPrice * service;
+  totalPrice = totalPrice + serviceCharge;
+  console.log(`Service Charge : $ ${serviceCharge}`);
+  // tax
+  let tax = 0.07;
+  let taxpayment = totalPrice * tax;
+  totalPrice = totalPrice + taxpayment;
+  console.log(`Tax amount : $ ${taxpayment}`);
+  // split Bill
+  let numberOfPeople = 2;
+  let split = totalPrice / numberOfPeople;
+  console.log(`The amount of each person : $ ${split}`);
+  return totalPrice;
 }
-console.log(`The total bill : $${total_Price}`);
-// service Charge
-let service = 0.1;
-let serviceCharge = total_Price * service;
-total_Price = total_Price + serviceCharge;
-console.log(`Service Charge : $ ${serviceCharge}`);
-// tax
-let tax = 0.07;
-let taxpayment = total_Price * tax;
-total_Price = total_Price + taxpayment;
-console.log(`Tax amount : $ ${taxpayment}`);
-// split Bill
-let number_of_people = 2;
-let split = total_Price / number_of_people;
-console.log(`The amount of each person : $ ${split}`);
+billCal(list);
