c966cc370e0e7eb600cbc282ee7ffd1b9e79149d
Lily <ssu627366@gmail.com>
2026-06-19 12:52:38 +0630

    new

--- tripCostCal.js
+++ tripCostCal.js
@@ -1,9 +1,19 @@
 let trip = { distance: 300, effcienty: 25, cost: 3.75 };
-function tripAmount(tripCost) {
-  let require = tripCost.distance / tripCost.effcienty;
-  let TripCost = require * tripCost.cost;
-  console.log(`Fuel needed : ${require} gallon`);
-  console.log(`The total cost of Trip : $${TripCost}`);
-  return TripCost;
+// function tripAmount(tripCost) {
+//   let require = tripCost.distance / tripCost.effcienty;
+//   let TripCost = require * tripCost.cost;
+//   console.log(`Fuel needed : ${require} gallon`);
+//   console.log(`The total cost of Trip : $${TripCost}`);
+//   return;
+// }
+// tripAmount(trip);
+function requireFuel(distance, effcienty) {
+  return distance / effcienty;
 }
-tripAmount(trip);
+function tripAmount(fuelNeed, cost) {
+  return fuelNeed * cost;
+}
+let newRequire = requireFuel(trip.distance, trip.effcienty);
+let newTripCost = tripAmount(newRequire, trip.cost);
+console.log(`Fuel needed : ${newRequire} gallon`);
+console.log(`The total cost of Trip : $${newTripCost}`);
